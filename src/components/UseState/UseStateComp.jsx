b0e9bfe3b171762f2504743c01eee2a2fcb86dd0
Gabykap <gabriacos@gmail.com>
2024-04-08 22:43:24 -0300

    Se agregó el useContext para ver las ultimas busquedas

--- src/components/UseState/UseStateComp.jsx
+++ src/components/UseState/UseStateComp.jsx
@@ -1,19 +1,43 @@
 import React, { useContext } from "react";
 import YugiohContext from "../context/YugiohContext.jsx"; // Asegúrate de importar correctamente el contexto
-
+import Alert from "react-bootstrap/Alert"; // Importa el componente de alerta de Bootstrap
+import Card from "react-bootstrap/Card"; // Importa el componente de tarjeta de Bootstrap
 const UseStateComp = () => {
-    const { search, setSearch } = useContext(YugiohContext);
+    const { search } = useContext(YugiohContext);
 
-    console.log(YugiohContext);
+    //boton eliminar
+    const handleDelete = (index) => {
+        search.splice(index, 1);
+    };
+
     return (
         <div className="container">
-            <h1>useState</h1>
-            <h2>Search: {search}</h2>
-            <input
-                type="text"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-            />
+            
+            {search.length > 0 && ( // Muestra las alertas solo si hay búsquedas
+                <Card>
+                    <Card.Header>
+                        Ultimas búsqueda...
+                    </Card.Header>
+                    <Card.Body>
+                    {
+                        search.length > 0 ? (
+                            search.map((item, index) => (
+                                item.length > 1 ? (
+                                    <Alert key={index} variant="warning">
+                                    <p>{item}</p>
+                                </Alert>
+                                ): null
+                            ))
+                        ): (
+                            <Alert variant="warning">
+                                <p>No hay búsquedas</p>
+                            </Alert>
+                        )
+                    }
+                    </Card.Body>
+                    
+                </Card>
+            )}
         </div>
     );
 };
